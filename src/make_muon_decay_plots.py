2a1693c28330910449faafc9e83860a891ca8d4f
Matthias W. Smith <mwsmith2@uw.edu>
2017-06-18 20:10:40 -0700

    Completes a revision of chapter two.

--- src/make_muon_decay_plots.py
+++ src/make_muon_decay_plots.py
@@ -23,19 +23,21 @@ def main():
     n_rest = y * y * (3 - 2 * y)
     a_rest = (2 * y - 1) / (3 - 2 * y)
 
+    # And the lab frame.
+    n_lab = (y - 1) * (4 * y**2 - 5 * y - 5) / 5
+    a_lab = (-8 * y**2 + y + 1) / (4 * y**2 - 5 * y - 5)
+
+    # Plot the rest frame.
     plt.subplot(1, 2, 1)
-    plt.plot(y, n_rest, label='N')
-    plt.plot(y, a_rest, label='A')
+    plt.plot(y, n_rest, label=r'$n(y)$')
+    plt.plot(y, a_rest, label=r'$a(y)$')
 
     plt.title('Rest Frame')
     plt.xlabel(r'y [$E/E_{max}$]')
     plt.grid(alpha=0.5)
     plt.legend()
 
-    # And the lab frame.
-    n_lab = (y - 1) * (4 * y**2 - 5 * y - 5) / 5
-    a_lab = (-8 * y**2 + y + 1) / (4 * y**2 - 5 * y - 5)
-
+    # And, the lab frame.
     plt.subplot(1, 2, 2)
     plt.plot(y, n_lab, label='N')
     plt.plot(y, a_lab, label='A')
@@ -44,6 +46,33 @@ def main():
     plt.xlabel(r'y [$E/E_{max}$]')
     plt.grid(alpha=0.5)
     plt.savefig('fig/muon-decay-distributions.pdf')
+
+    # Make similar plots with the FoM included now.
+    fom = n_lab * a_lab * np.abs(a_lab)
+    fom_lo = fom.cumsum()
+    fom_hi = fom[::-1].cumsum()[::-1]
+
+    plt.clf()
+    plt.subplot(1, 2, 1)
+    plt.plot(y, n_lab, label=r'$a(y)$')
+    plt.plot(y, a_lab, label=r'$n(y)$')
+
+    plt.title('Muon Decay Attributes')
+    plt.xlabel(r'y [$E/E_{max}$]')
+    plt.grid(alpha=0.5)
+    plt.legend()
+
+    plt.subplot(1, 2, 2)
+    plt.plot(y, fom_lo / fom_hi.max(), label=r'$\int_0^y n(y)a^2(y) dy$')
+    plt.plot(y, fom_hi / fom_hi.max(), label=r'$\int_y^1 n(y)a^2(y) dy$')
+
+    plt.title('Spin Correlation Figure of Merit')
+    plt.xlabel(r'y [$E/E_{max}$]')
+    plt.ylim([-1.1, 1.1])
+    plt.grid(alpha=0.5)
+    plt.legend()
+
+    plt.savefig('fig/muon-precession-fom.pdf')
 
     return 0
 
